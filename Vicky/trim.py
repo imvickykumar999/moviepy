f8bcc821aca012e6bb527608c36d1655b26d95c9
imvickykumar999 <imvickykumar999@gmail.com>
2021-07-04 19:32:21 +0530

    dark love_trim.mp4

---
+++ Vicky/trim.py
@@ -0,0 +1,9 @@
+
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip as vix
+
+# ffmpeg_extract_subclip("full.mp4", start_seconds, end_seconds, targetname="cut.mp4")
+
+original = "dark love.mp4"
+trimmed = original.split('.')[0]+'_trim.mp4'
+
+vix(original, 76, 300, targetname=trimmed)
